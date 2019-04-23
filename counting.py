6289886818de51f803e8c6e5d61b167b7c2b6382
Ondřej Wünsch <wunsch.ondrej@gmail.com>
2019-04-23 20:27:35 +0200

    mark 2
    100% original content

--- counting.py
+++ counting.py
@@ -1,86 +1,112 @@
 import tkinter as tk
-from tkinter import Label, Entry, Radiobutton, Button, END
-from random import randint
+from tkinter import Label, Button, Frame, Entry
+from functools import partial
+from random import randint, choice
 
-main = tk.Tk()
-operation = tk.StringVar()
+class Problem():
+    def __init__(self):
+        self.select_operation()
+        self.generate()
 
-def get_operation():
-    selection = str(operation.get())
-    operator.config(text=selection)
+    def insert_values(self):
+        lbl_a.config(text=self.a)
+        lbl_operation.config(text=self.operation)
+        lbl_b.config(text=self.b)
 
-Y = "JES"
+    def select_operation(self):
+        choose = ent_operators.get()
+        if choose:
+            self.operation = choice(choose)
+        else:
+            self.operation = choice("+-*/")
 
-def generate():
-    global Y
-    if operation.get() == "+":
-        A = randint(1, 99)
-        B = randint(1, 100-A)
-        Y = A +B
+    def generate(self):
+        if self.operation == "+":
+            a = randint(1, 99)
+            b = randint(1, 100-a)
+            y = a +b
 
-    if operation.get() == "-":
-        A = randint(0, 100)
-        B = randint(0, A)
-        Y = A - B
+        if self.operation == "-":
+            a = randint(0, 100)
+            b = randint(0, a)
+            y = a - b
 
-    if operation.get() == "*":
-        A = randint(0, 10)
-        B = randint(0, 10)
-        Y = A * B
+        if self.operation == "*":
+            a = randint(0, 10)
+            b = randint(0, 10)
+            y = a * b
 
-    if operation.get() == "/":
-        B = randint(1, 10)
-        Y = randint(1, 10)
-        A = B*Y
+        if self.operation == "/":
+            b = randint(1, 10)
+            y = randint(1, 10)
+            a = b * y
+        #nechtělo se mi ke všem připisovat self :P
+        self.a = a
+        self.b = b
+        self.y = y
 
-    ent_A.delete(0, END)
-    ent_A.insert(0, str(A))
-    ent_B.delete(0, END)
-    ent_B.insert(0,str(B))
+    def check(self):
+        answer = ent_answer.get()
+        if self.y == int(answer):
+            lbl_correct.config(text="Správně", bg="lightgreen")
+        else:
+            lbl_correct.config(text="Špatně", bg="pink")
+
+def new():
+    #jojo prasárna
+    global problem
+    lbl_correct.config(text="čekám", bg="white")
+    problem = Problem()
+    problem.insert_values()
 
 def check():
-    if Y == int(ent_result.get()):
-        lbl_correct.config(text="Spravně")
-    else:
-        lbl_correct.config(text="Špatně")
+    global problem
+    problem.check()
 
+main = tk.Tk()
 
-lbl_operace = Label(main, text=u"Operace: ").grid(row=0, column=0, columnspan=1)
+fr_operators = Frame(main)
+fr_operators.grid(row=0, column=0)
 
-radio_plus = Radiobutton(main, text="+", value="+", variable=operation, command=get_operation)
-radio_plus.grid(row=0, column=1)
+lbl_operators = Label(fr_operators, text="Vypište znaménka:")
+lbl_operators.grid(row=0, column=0)
 
-radio_minus = Radiobutton(main, text="-", value="-", variable=operation, command=get_operation)
-radio_minus.grid(row=0, column=2)
+ent_operators = Entry(fr_operators)
+ent_operators.grid(row=0, column=1)
 
-radio_times = Radiobutton(main, text="*", value="*", variable=operation, command=get_operation)
-radio_times.grid(row=0, column=3)
+#prasárna numero dos
+problem = Problem()
 
-radio_divide = Radiobutton(main, text="/", value="/", variable=operation, command=get_operation)
-radio_divide.grid(row=0, column=4)
+fr_buttons = Frame(main)
+fr_buttons.grid(row=1, column=0)
 
-ent_A= Entry(main, width=5)
-ent_A.grid(row=1, column=0)
+bt_new = Button(fr_buttons, text="Nový příklad", command=new)
+bt_new.grid(row=0, column=0)
 
-operator= Label(main, text="Vyber znamenko")
-operator.grid(row=1, column=1)
+bt_new = Button(fr_buttons, text="Kontrola", command=check)
+bt_new.grid(row=0, column=1)
 
-ent_B= Entry(main, width=5)
-ent_B.grid(row=1, column=2)
+lbl_correct = Label(fr_buttons, text="čekám")
+lbl_correct.grid(row=0, column=2)
 
-lbl_equals = Label(main, text="=", width=5)
-lbl_equals.grid(row=1, column=3)
+fr_problem = Frame(main)
+fr_problem.grid(row=2, column=0)
 
-ent_result = Entry(main, width=5)
-ent_result.grid(row=1, column=4)
+lbl_a = Label(fr_problem)
+lbl_a.grid(row=0, column=0)
 
-but_new=Button(main, text="Nový příklad", command=generate)
-but_new.grid(row=2, column=0)
+lbl_operation = Label(fr_problem)
+lbl_operation.grid(row=0, column=1)
 
-but_new=Button(main, text="Kontrola", command=check)
-but_new.grid(row=2, column=1)
+lbl_b = Label(fr_problem)
+lbl_b.grid(row=0, column=2)
 
-lbl_correct = Label(main, text="")
-lbl_correct.grid(row=2, column=2)
+lbl_equals = Label(fr_problem, text="=")
+lbl_equals.grid(row=0, column=3)
+
+ent_answer = Entry(fr_problem)
+ent_answer.grid(row=0, column=4)
+
+problem.insert_values()
 
 main.mainloop()
